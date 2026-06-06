9e18fd3abf85d3b846df2a7a7b9a6bbcbf767784
Gudipati Sreekar <sreekargidipati005@gmailcom>
2026-06-06 22:09:37 +0530

    remove yellow dots

--- popx-app/src/pages/Welcome.jsx
+++ popx-app/src/pages/Welcome.jsx
@@ -6,30 +6,7 @@ export default function Welcome() {
 
   return (
     <div className="welcome-page">
-      <div className="welcome-graphic">
-        {/* Decorative floating dots */}
-        {[
-          { top: '4%',  left: '28%' },
-          { top: '8%',  left: '78%' },
-          { top: '14%', left: '32%' },
-          { top: '16%', left: '56%' },
-          { top: '22%', left: '44%' },
-          { top: '26%', left: '58%' },
-          { top: '32%', left: '68%' },
-          { top: '38%', left: '74%' },
-          { top: '44%', left: '62%' },
-          { top: '52%', left: '44%' },
-          { top: '54%', left: '28%' },
-          { top: '50%', left: '8%'  },
-          { top: '60%', left: '18%' },
-        ].map((pos, i) => (
-          <span
-            key={i}
-            className="dot"
-            style={{ top: pos.top, left: pos.left }}
-          />
-        ))}
-      </div>
+      <div className="welcome-graphic"></div>
 
       <div className="welcome-content">
         <h1 className="welcome-title">Welcome to PopX</h1>
